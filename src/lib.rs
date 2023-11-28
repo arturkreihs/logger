b87791f8d24c35ef5716b81fdcdef1aced661ec3
Artur Kreihs <akreihs@ascs.pl>
2023-11-28 12:22:37 +0000

    printing only app name

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
 use std::io::Write;
 use std::time::Instant;
 use thiserror::Error;
+use std::path::Path;
 
 #[derive(Error, Debug)]
 pub enum LoggerError {
+    #[error("Can't get full path of application")]
+    FullPath,
+    #[error("File name can't be obtained")]
+    FileName,
+    #[error("Name is not valid UTF-8")]
+    NameStr,
 }
 
 pub fn init() -> Result<(), LoggerError> {
     let start_time = Instant::now();
-    let appname = std::env::args().next().unwrap_or_else(|| "app".into());
+    let fullpath = std::env::args()
+        .next()
+        .ok_or_else(|| LoggerError::FullPath)?;
+    let appname = Path::new(&fullpath)
+        .file_name()
+        .ok_or_else(|| LoggerError::FileName)?
+        .to_str()
+        .ok_or_else(|| LoggerError::NameStr)?
+        .to_owned();
+
     env_logger::Builder::from_env("LOGLEVEL")
         .format(move |buf, record| {
             let time = Instant::now()
@@ -24,5 +40,6 @@ pub fn init() -> Result<(), LoggerError> {
             )
         })
         .init();
+
     Ok(())
 }
